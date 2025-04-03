ea2a34996948b4cc5545cf7ccb86129654b4b784
kkien1903 <luuthikimyen2865@gmail.com>
2025-04-03 14:16:21 +0700

    Add files via upload

---
+++ lab05/img-hidden/encrypt.py
@@ -0,0 +1,34 @@
+import sys
+from PIL import Image
+
+def encode_image(image_path, message):
+    img= Image.open(image_path)
+    width, height = img.size
+    pixel_index=0
+    binary_message = ''.join(format(ord(i), '08b') for i in message)
+    binary_message += '1111111111111110'
+    data_index = 0
+    for row in range(height):
+        for col in range(width):
+            pixel = list(img.getpixel((col, row)))
+            
+            for color_channel in range(3):
+                if data_index < len(binary_message):
+                    pixel[color_channel] = int(format(pixel[color_channel], '08b')[:-1] + binary_message[data_index], 2)
+                    data_index += 1
+            img.putpixel((col, row), tuple(pixel))
+            if data_index >= len(binary_message):
+                break
+    encoded_image_path = 'encoded_image.png'
+    img.save(encoded_image_path)
+    print("Ảnh đã được mã hóa và lưu thành công tại: {}".format(encoded_image_path))
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python3 {} <image_path> <message>".format(sys.argv[0]))
+        return
+    image_path = sys.argv[1]
+    message = sys.argv[2]
+    encode_image(image_path, message)
+if __name__ == "__main__":
+    main()
